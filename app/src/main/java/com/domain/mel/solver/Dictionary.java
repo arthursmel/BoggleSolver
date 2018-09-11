6af8ef9f06ced158ce133655c30ee50e13156a3b
arthursmel <arthurme@tcd.ie>
2018-09-11 13:15:14 +0100

    Testing building a trie

--- app/src/main/java/com/domain/mel/solver/Dictionary.java
+++ app/src/main/java/com/domain/mel/solver/Dictionary.java
@@ -22,31 +22,68 @@ public class Dictionary {
 
     private static final String TAG = "Dictionary";
     private static final String DICT_FILE_NAME = "dict.txt";
+    private static final char POP_STACK = '.';
+
     private TrieNode root;
 
 
     public Dictionary(Context context) throws FileNotFoundException, IOException {
 
         Stack<TrieNode> stack = new Stack<>();
-
         this.root = new TrieNode();
-        TrieNode prevNode = root;
-        TrieNode curNode = null;
-
+        TrieNode prevNode = null;
+        TrieNode curNode = this.root;
 
         BufferedReader inputStream = new BufferedReader(new InputStreamReader(
                 context.getAssets().open(DICT_FILE_NAME)
         ));
-        int curChar;
 
+        int curChar;
         while ((curChar = inputStream.read()) != -1) {
 
             Log.d(TAG, " char: " + (char) curChar);
 
+            if (isLowercaseAlphabetical((char) curChar)) {
+                prevNode = curNode;
+                curNode = new TrieNode((char) curChar);
+                stack.push(curNode);
+                prevNode.children.add(curNode);
+
+            } else if ((char) curChar == POP_STACK) {
+                // If the current character indicated to pop the stack
+                prevNode = stack.pop();
+                curNode = stack.peek();
+
+            } else {
+                // Otherwise invalid character
+
+            }
+
         }
 
+        this.DFS(root);
         inputStream.close();
 
+    }
+
+
+    public boolean isPartialWord() {
+        return true;
+    }
+
+    public boolean isWord() {
+        return true;
+    }
+
+    private void DFS(TrieNode node) {
+        for (TrieNode n : node.getChildren()) {
+            Log.d(TAG, n.toString());
+            this.DFS(n);
+        }
+    }
+
+    private static boolean isLowercaseAlphabetical(char c) {
+        return c >= (int) 'a' && c <= (int) 'z';
     }
 
 
@@ -71,8 +108,33 @@ public class Dictionary {
             this.children = new ArrayList<>();
         }
 
+        boolean isLeaf() {
+            return this.children.isEmpty();
+        }
+
         void addChild(TrieNode node) {
             children.add(node);
+        }
+
+        char getLetter() {
+            return this.letter;
+        }
+
+        ArrayList<TrieNode> getChildren() {
+            return this.children;
+        }
+
+
+        @Override
+        public String toString() {
+            StringBuilder str = new StringBuilder();
+            str.append(this.letter);
+            str.append(" ");
+            for (TrieNode n : this.children) {
+                str.append(n.getLetter());
+                str.append(", ");
+            }
+            return str.toString();
         }
 
 
